8a22295aa5dfdddfe7aff75c82cc22852d1629fc
Khalil Hajlaoui <98230912+KhajlaouiBdeB@users.noreply.github.com>
2026-08-11 14:34:31 -0400

    [Game] Fix repeater infinite loop

--- Source/Game/BehaviourTree/Composites.h
+++ Source/Game/BehaviourTree/Composites.h
@@ -48,6 +48,8 @@ namespace BehaviourTree
                 if (state != NodeState::FAILURE)
                     return state;
             }
+
+            _currentIndex = 0;
             return NodeState::FAILURE;
         }
     };
